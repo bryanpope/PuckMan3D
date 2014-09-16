0af26c4f83cd1c249dc3c0272d6bb606ba1b12a5
Andrew Best <andrew.c.best@gmail.com>
2014-09-15 23:11:02 -0700

    Warnings galore!
    Compiles just fine, most of the pathfinding is in, unable to test at
    this exact moment.

--- Ghost.cpp
+++ Ghost.cpp
@@ -1,6 +1,4 @@
 #include "Ghost.h"
-#include "GeometryGenerator.h"
-#include "Vertex.h"
 
 Ghost::Ghost()
 {
@@ -20,4 +18,112 @@ Ghost::~Ghost()
 void Ghost::Update()
 {
 	
+}
+
+std::vector<Node*> Ghost::FindPath(Node* start, Node* goal)
+{
+	std::vector<Node*> path;
+
+	mClosedList.clear();
+	mOpenList.clear();
+
+	Node* childNode;
+	Node* currentNode;
+	currentNode->combineNode(*currentNode, start);
+
+	mOpenList.push_back(start);
+	start->isOpen = true;
+
+	while (*currentNode != *goal)
+	{
+		for (int i = 0; i < mOpenList.size(); ++i)
+		{
+			if (mOpenList[i]->fCost <= currentNode->fCost)
+			{
+				currentNode->combineNode(*currentNode, mOpenList[i]);
+			}
+		}
+		//Did we reach the end? If so, stop
+		if (*currentNode == *goal)
+		{
+			break;
+		}
+		//If not, we continue and remove the currentNode from the openList
+			//Because the currentNode isn't in the openList, we will simply set isOpen to false
+			//and isClosed to true
+		currentNode->isOpen = false;
+		mClosedList.push_back(currentNode);
+		currentNode->isClosed = true;
+
+		//Get adjacent walkable tiles
+		for (int col = -1; col < 2; ++col)
+		{
+			for (int row = -1; row < 2; ++row)
+			{
+				//Skip if this lands on currentNode
+				if (col == 0 && row == 0)
+				{
+					continue;
+				}
+				//If not, get the node
+					//childNode = getNode(currentNode->getCol() + col, currentNode->getRow() + row);
+
+				if (childNode->isClosed || !childNode->isWalkable)
+				{
+					continue;
+				}
+
+				//If we hit a corner
+				if (col != 0 && row != 0)
+				{
+					//Check surroundings for walkable tiles and if in closed list
+					/*if (!isWalkable(currentNode->getCol(), currentNode->getRow() + row) || getNode(currentNode->getCol(), currentNode->getRow() + row)->closed)
+					{
+						continue;
+					}
+					if (!isWalkable(currentNode->getCol() + col, currentNode->getRow()) || getNode(currentNode->getCol() + col, currentNode->getRow())->closed)
+					{
+						continue;
+					}*/
+				}
+
+				//If it's already in the open list
+				if (childNode->isOpen)
+				{
+					//If it has a wroste g score than the one that pass through the current point
+						// then its path is improved when it's parent is the current point
+					if (childNode->gCost > childNode->gCost + currentNode->gCost)
+					{
+						childNode->setParent(currentNode);
+						//childNode->
+					}
+				}
+				else
+				{
+					//Add it to the openList with the current node as the parent
+					mOpenList.push_back(childNode);
+					childNode->isOpen = true;
+					childNode->setParent(currentNode);
+					childNode->calculateCosts(goal);
+				}
+			}
+		}
+	}
+	//Reset everything
+	for (int i = 0; i < mOpenList.size(); ++i)
+	{
+		mOpenList[i]->isOpen = false;
+	}
+	for (int i = 0; i < mClosedList.size(); ++i)
+	{
+		mClosedList[i]->isClosed = false;
+	}
+
+	//Populate the path vector
+	while (currentNode->mParent != NULL && currentNode != start)
+	{
+		path.push_back(currentNode);
+		currentNode = currentNode->getParent();
+	}
+	return path;
 }
--- Ghost.h
+++ Ghost.h
@@ -1,5 +1,105 @@
 #pragma once
 #include "d3dUtil.h"
+#include <cmath>
+
+struct Node
+{
+	int row;
+	int col;
+	int gCost;
+	int hCost;
+	int fCost;
+	bool isWalkable;
+	bool isOpen;
+	bool isClosed;
+	std::string facing;
+	Node* mParent;
+
+	Node combineNode(Node initial, Node* target)
+	{
+		initial.col = target->col;
+		initial.row = target->row;
+		initial.gCost = target->gCost;
+		initial.fCost = target->fCost;
+		initial.facing = target->facing;
+
+		return initial;
+	}
+
+	Node* getParent()
+	{
+		return mParent;
+	}
+
+	int getGCost(Node* n)
+	{
+		return n->gCost + ((col == n->col || row == n->row) ? 10 : 14);
+	}
+
+	int getHCost(Node* n)
+	{
+		return (abs(n->col - col) + abs(n->row - row)) * 10;
+	}
+
+	int getRow()
+	{
+		return row;
+	}
+
+	int getCol()
+	{
+		return col;
+	}
+
+	void setParent(Node* parent)
+	{
+		mParent = parent;
+	}
+
+	void calculateCosts(Node* goal)
+	{
+		gCost = getGCost(mParent);
+		hCost = getHCost(mParent);
+		fCost = gCost + hCost;
+	}
+
+	//May not need these overloads, currently work in progress
+	bool operator==(const Node& rhs)
+	{
+		if (row == rhs.row && col == rhs.col && gCost == rhs.gCost && fCost == rhs.fCost && facing == rhs.facing)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool operator!=(const Node& rhs)
+	{
+		if (row != rhs.row && col != rhs.col)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool operator=(const Node& rhs)
+	{
+		row = rhs.row;
+		col = rhs.col;
+		gCost = rhs.gCost;
+		fCost = rhs.fCost;
+		facing = rhs.facing;
+		isWalkable = rhs.isWalkable;
+		isOpen = rhs.isOpen;
+		isClosed = rhs.isOpen;
+	}
+};
 
 class Ghost
 {
@@ -16,6 +116,17 @@ protected:
 	XMFLOAT3 mPos; //Originally set as XMVectorSet(0.0f, 0.75f, 3.5f, 0.0f)
 	XMFLOAT3 mVel; //Originally set as XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f)
 	float mRadius; //Originally set as 0.75f
+
+protected:
+	//Pathfinding stuff -- make different class or keep as is?
+	std::vector<Node*> mOpenList;
+	std::vector<Node*> mClosedList;
+
+	std::vector<Node*> FindPath(Node* start, Node* goal);
+	void AddChild(Node* childNode, Node* currNode, Node* goal);
+	float GetDistance(Node* currNode, Node* parent);
+	bool InClosedList(Node* n);
+	bool InOpenList(Node* n);
 
 public:
 	Ghost();
