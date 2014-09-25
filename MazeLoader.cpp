cba5fe66205251f0e9e7daea35f0a97a4db23897
Andrew Best <andrew.c.best@gmail.com>
2014-09-25 14:27:58 -0400

    IT MOVES!
    IT'S ALIIIIIIIIIIIIVE

--- MazeLoader.cpp
+++ MazeLoader.cpp
@@ -367,7 +367,7 @@ bool MazeLoader::IsBlocked(UINT row, UINT col)
 {
 	UINT index = (row * mMazeWidth) + col;
 
-	return ((mMazeElements[index] == ME_WALL) || (mMazeElements[index] == ME_NOTHING));
+	return ((mMazeElements[index] == ME_WALL) || (mMazeElements[index] == ME_BLANK));
 }
 
 void MazeLoader::SetPacManPos(FXMVECTOR pos, UINT index)
